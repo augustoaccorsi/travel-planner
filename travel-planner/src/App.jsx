8464cde830f591f1cfa9686a0a92705ce4c35bda
augustoaccorsi <augusto.acorsi@gmail.com>
2024-07-12 08:43:39 -0300

    add tailwind and first layput

--- travel-planner/src/App.jsx
+++ travel-planner/src/App.jsx
@@ -1,5 +1,34 @@
 function App() {
-    return <h1>Hello Word</h1>;
+    return (
+        <div className="bg-zinc-950 text-zinc-50 antialiased">
+            <div className="h-screen flex items-center justify-center">
+                <div className="max-w-3xl w-full px-6 text-center space-y-10">
+                    <p className="text-zinc-300 text-lg">
+                        Convide seus amigos e planeje sua próxima viagem!
+                    </p>
+
+                    <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center">
+                        <input placeholder="Para onde você vai?"></input>
+                        <input placeholder="Quando"></input>
+                        <button>Continuar</button>
+                    </div>
+
+                    <p className="text-zinc-500 text-sm">
+                        Ao planejar sua viagem pela plann.er você
+                        automaticamente concorda <br /> com nossos{' '}
+                        <a className="text-zinc-300 underline" href="#">
+                            termos de uso
+                        </a>{' '}
+                        e{' '}
+                        <a className="text-zinc-300 underline" href="#">
+                            políticas de privacidade
+                        </a>
+                        .
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
