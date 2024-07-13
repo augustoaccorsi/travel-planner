e585f2e29a3faca86c20c1d4139fb53ce90fad53
augustoaccorsi <augusto.acorsi@gmail.com>
2024-07-13 14:47:51 -0300

    Add form data to add invited users

--- travel-planner/src/App.jsx
+++ travel-planner/src/App.jsx
@@ -1,6 +1,40 @@
-import { MapPin, Calendar, ArrowRight } from 'lucide-react';
+import {
+    MapPin,
+    Calendar,
+    ArrowRight,
+    UserRoundPlus,
+    Settings2,
+    X,
+    AtSign,
+    Plus,
+} from 'lucide-react';
+import { useState } from 'react';
 
 function App() {
+    const [isGuestInputOpen, setIsGuestInputOpen] = useState(false);
+    const [isGuestModalOpen, setIsGuestModalOpen] = useState(false);
+    const [invitedUsers, setInvitedUsers] = useState([]);
+
+    const handleAddInvetedUser = (event) => {
+        event.preventDefault();
+
+        const data = new FormData(event.currentTarget);
+        const userEmail = data.get('email').toString();
+
+        if (!userEmail || invitedUsers.includes(userEmail)) {
+            return;
+        }
+
+        setInvitedUsers([...invitedUsers, userEmail]);
+        event.currentTarget.reset();
+    };
+
+    const handleRemoveInvited = (indexToRemove) => {
+        setInvitedUsers(
+            invitedUsers.filter((_, index) => indexToRemove !== index)
+        );
+    };
+
     return (
         <div className="bg-zinc-950 text-zinc-50 antialiased bg-patter bg-no-repeat bg-center">
             <div className="h-screen flex items-center justify-center">
@@ -12,28 +46,67 @@ function App() {
                         </p>
                     </div>
 
-                    <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center gap-3">
-                        <div className="flex items-center gap-2 flex-1">
-                            <MapPin className="size-5 text-zinc-400" />
-                            <input
-                                placeholder="Para onde você vai?"
-                                className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
-                            ></input>
-                        </div>
-                        <div className="flex items-center gap-2">
-                            <Calendar className="size-5 text-zinc-400" />
-                            <input
-                                placeholder="Quando?"
-                                className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none"
-                            ></input>
+                    <div className="space-y-4">
+                        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center gap-3">
+                            <div className="flex items-center gap-2 flex-1">
+                                <MapPin className="size-5 text-zinc-400" />
+                                <input
+                                    disabled={isGuestInputOpen}
+                                    placeholder="Para onde você vai?"
+                                    className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                                ></input>
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <Calendar className="size-5 text-zinc-400" />
+                                <input
+                                    disabled={isGuestInputOpen}
+                                    placeholder="Quando?"
+                                    className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none"
+                                ></input>
+                            </div>
+
+                            <div className="w-px h-6 bg-zinc-800 " />
+
+                            {!isGuestInputOpen ? (
+                                <button
+                                    className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400"
+                                    onClick={() => setIsGuestInputOpen(true)}
+                                >
+                                    Continuar
+                                    <ArrowRight className="text-lime-950 size-5" />
+                                </button>
+                            ) : (
+                                <button
+                                    onClick={() => setIsGuestInputOpen(false)}
+                                    className="bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700"
+                                >
+                                    Alterar local/data
+                                    <Settings2 className="text-zinc-400 size-5" />
+                                </button>
+                            )}
                         </div>
 
-                        <div className="w-px h-6 bg-zinc-800 " />
+                        {isGuestInputOpen ? (
+                            <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center gap-3">
+                                <button
+                                    onClick={() => setIsGuestModalOpen(true)}
+                                    type="button"
+                                    className="flex items-center gap-2 flex-1"
+                                >
+                                    <UserRoundPlus className="size-5 text-zinc-400" />
+                                    <span className="bg-transparent text-lg text-zinc-400 outline-none flex-1 text-left">
+                                        Quem estará na viagem?
+                                    </span>
+                                </button>
 
-                        <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
-                            Continuar
-                            <ArrowRight className="text-lime-950 size-5" />
-                        </button>
+                                <div className="w-px h-6 bg-zinc-800 " />
+
+                                <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+                                    Confirmar Viagem
+                                    <ArrowRight className="text-lime-950 size-5" />
+                                </button>
+                            </div>
+                        ) : null}
                     </div>
 
                     <p className="text-zinc-500 text-sm">
@@ -50,6 +123,76 @@ function App() {
                     </p>
                 </div>
             </div>
+
+            {isGuestModalOpen && (
+                <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+                    <div className="w-[640px] rounded-xl py-5 px-6 bg-zinc-900 space-y-5">
+                        <div className="space-y-2">
+                            <div className="flex items-center justify-between">
+                                <h2 className="text-lg font-semibold">
+                                    Selecionar Convidados
+                                </h2>
+                                <button
+                                    onClick={() => setIsGuestModalOpen(false)}
+                                >
+                                    <X className="size-5 text-zinc-400"></X>
+                                </button>
+                            </div>
+                            <p className="text-sm text-zinc-400">
+                                Os convidados irão receber e-mails para
+                                confirmar a participação na viagem.
+                            </p>
+                        </div>
+                        {invitedUsers.length > 0 ? (
+                            <div className="flex flex-wrap gap-2">
+                                {' '}
+                                {invitedUsers.map((user, index) => (
+                                    <div
+                                        key={index}
+                                        className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2"
+                                    >
+                                        <span className="text-zinc-300">
+                                            {user}
+                                        </span>
+                                        <button
+                                            type="button"
+                                            onClick={() =>
+                                                handleRemoveInvited(index)
+                                            }
+                                        >
+                                            <X className="size-4 text-zinc-400" />
+                                        </button>
+                                    </div>
+                                ))}
+                            </div>
+                        ) : null}
+
+                        <div className="w-full h-px bg-zinc-800" />
+
+                        <form
+                            onSubmit={handleAddInvetedUser}
+                            className="p-2.5 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2"
+                        >
+                            <div className="px-2 flex items-center flex-1 gap-2">
+                                <AtSign className="text-zinc-400 size-5" />
+                                <input
+                                    name="email"
+                                    type="email"
+                                    placeholder="Digite o e-mail do convidado"
+                                    className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                                />
+                            </div>
+                            <button
+                                type="submit"
+                                className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400"
+                            >
+                                Convidar
+                                <Plus className="text-lime-950 size-5" />
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
